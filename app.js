f4e46dd8a2166f20e0ebb066c4a42f0685116bfa
Johnny <johnnycalderondeveloper@gmail.com>
2019-06-28 14:38:00 +0200

    repositioning images

--- app.js
+++ app.js
@@ -50,64 +50,6 @@ const cardsArray = [
   ]
 
 
-
-// let gameGrid = cardsArray.concat(cardsArray);
-// let firstGuess = '';
-// let secondGuess = '';
-// let count = 0;
-// let previousTarget = null
-
-
-// gameGrid.sort(() => 0.5 - Math.random())
-
-
-// const game = document.getElementById('game');
-// const grid = document.createElement('section');
-// grid.setAttribute('class', 'grid');
-// game.appendChild(grid);
-
-// gameGrid.forEach(item => {
-//   const card = document.createElement('div');
-//   card.classList.add('card');
-//   card.dataset.name = item.name;
-//   card.style.backgroundImage = `url(${item.img})`;
-//   grid.appendChild(card);
-// });
-
-// // Add event listener to grid
-// grid.addEventListener('click', function(event) {
-//   //...
-//   if (count < 2) {
-//     count++
-//     if (count === 1) {
-//       // Assign first guess
-//       firstGuess = clicked.dataset.name
-//       clicked.classList.add('selected')
-//     } else {
-//       // Assign second guess
-//       secondGuess = clicked.dataset.name
-//       clicked.classList.add('selected')
-//     }
-//     // If both guesses are not empty...
-//     if (firstGuess !== '' && secondGuess !== '') {
-//       // and the first guess matches the second match...
-//       if (firstGuess === secondGuess) {
-//         // run the match function
-//         match()
-//       }
-//     }
-//   }
-// })
-
-// // Add match CSS
-// const match = () => {
-//   var selected = document.querySelectorAll('.selected')
-//   selected.forEach(card => {
-//     card.classList.add('match')
-//   })
-// }
-
-
 let gameGrid = cardsArray.concat(cardsArray);
 gameGrid.sort(() => 0.5 - Math.random());
 
