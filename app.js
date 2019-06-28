e78462d5f207f4d7e089bd1ade7ca000c74b3d66
Johnny <johnnycalderondeveloper@gmail.com>
2019-06-28 10:19:49 +0200

    adding images, creating a section element ith javascript

---
+++ app.js
@@ -0,0 +1,60 @@
+const cardsArray = [
+    {
+      name: 'cop',
+      img: '/img/cop.jpg',
+    },
+    {
+      name: 'esse',
+      img: '/img/esse.jpg',
+    },
+    {
+      name: 'firetruck',
+      img: '/img/firetruck.jpg',
+    },
+    {
+      name: 'high',
+      img: '/img/high.jpg',
+    },
+    {
+      name: 'mcqueen',
+      img: '/img/mcqueen.jpg',
+    },
+    {
+      name: 'papi',
+      img: '/img/papi.jpg',
+    },
+    {
+      name: 'purple',
+      img: '/img/purple.jpg',
+    },
+    {
+      name: 'retard',
+      img: '/img/retard.jpg',
+    },
+    {
+      name: 'stripweathers',
+      img: '/img/stripweathers.jpg',
+    },
+    {
+      name: 'trailertruck',
+      img: '/img/trailertruck.jpg',
+    },
+    {
+      name: 'verde',
+      img: '/img/verde.jpg',
+    },
+    {
+      name: 'woman',
+      img: '/img/woman.jpg',
+    },
+  ]
+
+
+  const game = document.getElementById('game');
+  const grid = document.createElement('section')
+  grid.setAttribute('class', 'grid')
+
+  console.log(document.all)
+
+  console.log(grid)
+  console.dir(document)
